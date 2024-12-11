9c43fc33ce06688449a831093fe308e2bee6a557
tucecifci <tucecifcii@gmail.com>
2024-12-11 15:17:18 +0300

    title added

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function TodoItem({input, setInputValue, addTodo}) {
   return (
     <div>
+        <h1>To-Do App</h1>
         <input type="text" 
         placeholder='What should ı do'
         value={input}
