c471e2ccf9b54eb71cec00c8118f82d11adb1ef9
tucecifci <tucecifcii@gmail.com>
2024-12-16 13:54:48 +0300

    delete function added

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FaTrash } from "react-icons/fa";
 
-
-function TodoList({todos}) {
-  return (
-    <div  className='container'>
-    {todos.map((todo, index) => (
-        <div className='todo-item' key={index}>{todo} </div>
-    ))}
-    </div>
-  )
+function TodoList({ todos, setTodos }) {
+const deleteTodo = (indexToDelete) =>{
+const updatedTodos = todos.filter((_, index) => index!== indexToDelete);
+setTodos(updatedTodos);
 }
 
-export default TodoList
+  return (
+    <div className="container">
+      {todos.map((todo, index) => (
+        <div className="todo-item" key={index}>
+          <input type="checkbox" />
+          <span>{todo} </span>
+          <FaTrash onClick={() => deleteTodo(index)} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TodoList;
